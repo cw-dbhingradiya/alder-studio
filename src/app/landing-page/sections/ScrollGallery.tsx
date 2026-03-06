ee7d71ef557627669545a813c308c2e0bb688b51
cw-dbhingradiya <dbhingradiya@codewinglet.com>
2026-03-06 15:59:41 +0530

    feat: add new gallery images and update ScrollGallery component

--- src/app/landing-page/sections/ScrollGallery.tsx
+++ src/app/landing-page/sections/ScrollGallery.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const images = [
   {
-    src: "/furniture/furniture_01.jpg",
+    src: "/furniture/gallery_01.jpg",
     imageClass: "-rotate-y-90",
     style: {
       top: "calc(49.6241% - 107.5px)",
@@ -13,24 +13,24 @@ const images = [
     },
   },
   {
-    src: "/furniture/furniture_02.jpg",
+    src: "/furniture/gallery_02.jpg",
     wrapperClass: "rotate-y-45",
-    imageClass: "-top-[82px] -left-[161px] -rotate-y-90",
+    imageClass: "top-[18px] -left-[161px] -rotate-y-90",
   },
   {
-    src: "/furniture/furniture_03.jpg",
+    src: "/furniture/gallery_03.jpg",
     wrapperClass: "rotate-y-90",
-    imageClass: "-top-[82px] -left-[161px] -rotate-y-90",
+    imageClass: "top-[58px] -left-[161px] -rotate-y-90",
   },
   {
-    src: "/furniture/furniture_04.jpg",
+    src: "/furniture/gallery_04.jpg",
     wrapperClass: "rotate-y-135",
-    imageClass: "-top-[82px] -left-[161px] -rotate-y-90",
+    imageClass: "top-[98px] -left-[161px] -rotate-y-90",
   },
   {
-    src: "/furniture/furniture_05.jpg",
+    src: "/furniture/gallery_05.jpg",
     wrapperClass: "rotate-y-180",
-    imageClass: "-top-[82px] -left-[161px] -rotate-y-90",
+    imageClass: "top-[138px] -left-[161px] -rotate-y-90",
   },
 ];
 
@@ -46,49 +46,44 @@ const ScrollGallery = () => {
   const rotateY = useTransform(scrollYProgress, [0.4, 0.6], [90, -90]);
 
   return (
-    <div className="flex w-full justify-center bg-black">
-      <section
-        ref={sectionRef}
-        className="relative flex w-full max-w-[1480px] flex-col items-center justify-start gap-[10px] overflow-hidden"
-      >
-        <div className="relative flex w-full flex-col items-center justify-center">
-          <div className="sticky top-0 z-1 flex h-screen w-full flex-col items-center justify-center gap-[36px]">
-            <div className="relative flex h-px w-full flex-1 flex-col items-center justify-center gap-[10px] overflow-hidden">
-              <motion.div
-                className="relative flex h-min w-min flex-col items-center justify-center transform-3d opacity-100 will-change-transform"
-                style={{
-                  translateY,
-                  rotateY,
-                  transformPerspective: 1200,
-                }}
+    <section ref={sectionRef} className="bg-black relative">
+      <div className="sticky top-0 z-1 flex h-screen w-full flex-col items-center justify-center gap-[36px]">
+        <div className="relative flex h-px w-full flex-1 flex-col items-center justify-center gap-[10px] overflow-hidden pb-[350px]">
+          <motion.div
+            className="relative flex h-min w-min flex-col items-center justify-center transform-3d opacity-100 will-change-transform"
+            style={{
+              translateY,
+              rotateY,
+              transformPerspective: 1200,
+            }}
+          >
+            {images.map((img, i) => (
+              <div
+                key={i}
+                className={`relative flex h-[151px] w-[865px] flex-row items-center justify-center gap-[10px] transform-3d ${img.wrapperClass}`}
               >
-                {images.map((img, i) => (
-                  <div
-                    key={i}
-                    className={`relative flex h-[151px] w-[865px] flex-row items-center justify-center gap-[10px] transform-3d ${img.wrapperClass}`}
-                  >
-                    <div
-                      className={`absolute h-[215px] w-[320px] ${img.imageClass}`}
-                      style={img.style}
-                    >
-                      <div className="relative flex h-full w-full cursor-pointer items-center justify-center overflow-hidden">
-                        <Image
-                          src={img.src}
-                          className="h-full w-full object-cover rounded-lg"
-                          width={320}
-                          height={215}
-                          alt={`Image ${i + 1}`}
-                        />
-                      </div>
-                    </div>
+                <div
+                  className={`absolute h-[215px] w-[320px] ${img.imageClass}`}
+                  style={img.style}
+                >
+                  <div className="relative flex h-full w-full cursor-pointer items-center justify-center overflow-hidden">
+                    <Image
+                      src={img.src}
+                      className="h-full w-full object-cover rounded-lg"
+                      width={640}
+                      height={430}
+                      alt={`Image ${i + 1}`}
+                      sizes="320px"
+                      quality={100}
+                    />
                   </div>
-                ))}
-              </motion.div>
-            </div>
-          </div>
+                </div>
+              </div>
+            ))}
+          </motion.div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
